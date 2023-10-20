bb34cda7bb5cf36972c1234679d917fa5e283de3
Wesley Crosslin <136374861+Weskc12@users.noreply.github.com>
2023-10-20 20:32:53 +0000

    final

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let domainGen = [];
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domain = [".com", ".us"];
+
+  for (let a = 0; a < pronoun.length; a++) {
+    for (let b = 0; b < adj.length; b++) {
+      for (let c = 0; c < noun.length; c++) {
+        for (let d = 0; d < domain.length; d++) {
+          domainGen.push(pronoun[a] + adj[b] + noun[c] + domain[d]);
+        }
+      }
+    }
+  }
+  return;
 };
+console.log(domainGen);
